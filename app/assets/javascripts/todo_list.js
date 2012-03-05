a47d9024ebe7fe4d35551dff90db938f8c687728
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-03-05 14:03:59 +0100

    Simple spec for Tasks collection.

--- app/assets/javascripts/todo_list.js
+++ app/assets/javascripts/todo_list.js
@@ -1,6 +1,8 @@
 //= require_self
 //= require_tree ./models
+//= require_tree ./collections
 
 window.TodoList = {
-  Models: {}
+  Models: {},
+  Collections: {}
 };
---
+++ spec/javascripts/collections/tasks_spec.js
@@ -0,0 +1,7 @@
+describe('TodoList.Collections.Tasks', function() {
+
+  it('should be defined', function() {
+    expect(TodoList.Collections.Tasks).toBeDefined();
+  });
+
+});
