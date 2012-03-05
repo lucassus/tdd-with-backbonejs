8c91e41b65ecf51037dca36b4ab26d94afebdd90
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-03-05 11:30:38 +0100

    Setup jasminerice and guard.

---
+++ spec/javascripts/sample_spec.js
@@ -0,0 +1,9 @@
+describe('Sample spec', function() {
+  it('should pass', function() {
+    expect(true).toBeTruthy();
+  });
+
+  it('should fail', function() {
+    expect(true).toBeFalsy();
+  });
+});
