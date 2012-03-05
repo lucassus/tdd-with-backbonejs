77853d0dc77f99d87a91845d6daf0a61ae22ce06
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-03-05 12:00:16 +0100

    Simple spec for Task model.

---
+++ spec/javascripts/models/task_spec.js
@@ -0,0 +1,7 @@
+describe('TodoList.Models.Task', function() {
+
+  it('should be defined', function() {
+    expect(TodoList.Models.Task).toBeDefined();
+  });
+
+});
--- spec/javascripts/sample_spec.js
+++ spec/javascripts/sample_spec.js
@@ -2,8 +2,4 @@ describe('Sample spec', function() {
   it('should pass', function() {
     expect(true).toBeTruthy();
   });
-
-  it('should fail', function() {
-    expect(true).toBeFalsy();
-  });
 });
