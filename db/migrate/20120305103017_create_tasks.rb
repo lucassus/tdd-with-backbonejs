350d8e6d724e7c31a6c645cbc1399e569e518466
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-03-05 11:42:48 +0100

    Fix migration.

--- db/migrate/20120305103017_create_tasks.rb
+++ db/migrate/20120305103017_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :name, :null => false
-      t.boolean :complete, :null => false, :default => true
+      t.boolean :complete, :null => false, :default => false
 
       t.timestamps
     end
