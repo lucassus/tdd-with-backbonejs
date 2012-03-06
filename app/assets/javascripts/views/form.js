912168ea83998ec09eed519800257a6e21e6e890
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-03-06 13:29:45 +0100

    Refactor Form#submit method.

--- app/assets/javascripts/views/form.js
+++ app/assets/javascripts/views/form.js
@@ -4,23 +4,29 @@ TodoList.Views.Form = Backbone.View.extend({
     'click button.submit': 'submit'
   },
 
+  initialize: function() {
+    this.collection.on('add', this.resetForm, this);
+  },
+
   submit: function(event) {
     if (event) event.preventDefault();
 
     var attributes = this.getAttributes();
+    var task = new TodoList.Models.Task(attributes);
+    task.on('error', this.handleErrors, this);
 
-    var self = this;
-    this.collection.create(attributes, {
-      success: function() {
-        self.$("input[name='task[name]']").val('');
-      },
-      error: function(task, response) {
-        var errors = JSON.parse(response.responseText).errors;
-        if (errors.name) {
-          alert("Task name " + errors.name);
-        }
-      }
-    });
+    this.collection.create(task, { wait: true });
+  },
+
+  resetForm: function() {
+    this.$("input[name='task[name]']").val('');
+  },
+
+  handleErrors: function(task, response) {
+    var errors = JSON.parse(response.responseText).errors;
+    if (errors.name) {
+      alert("Task name " + errors.name);
+    }
   },
 
   getAttributes: function() {
