aafcd1fe00ea76ce0d043de0cda949a317862199
Lukasz Bandzarewicz <lucassus@gmail.com>
2012-03-07 15:14:14 +0100

    Remove sample spec.

--- spec/javascripts/sample_spec.js
+++
@@ -1,9 +0,0 @@
-describe('Sample spec', function() {
-  it('should pass', function() {
-    expect(true).toBeTruthy();
-  });
-
-  it('should fail', function() {
-    expect(true).toBeFalsy();
-  });
-});
